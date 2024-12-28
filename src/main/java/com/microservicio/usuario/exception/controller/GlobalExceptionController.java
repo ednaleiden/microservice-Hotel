d46c993b94f2f8bf1aecfc979924a9f54572bba9
ednaleiden <ednadupont@hotmail.com>
2024-12-28 09:37:31 -0500

    implement controller

---
+++ src/main/java/com/microservicio/usuario/exception/controller/GlobalExceptionController.java
@@ -0,0 +1,25 @@
+package com.microservicio.usuario.exception.controller;
+
+import com.microservicio.usuario.exception.ResourceNotFoundException;
+import com.microservicio.usuario.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionController {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse> handlerResourceNotFoundException(ResourceNotFoundException resourceNotFoundException){
+        String message = resourceNotFoundException.getMessage();
+
+        ApiResponse response = new ApiResponse().builder()
+                .message(message)
+                .success(true)
+                .status(HttpStatus.NOT_FOUND)
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+}
